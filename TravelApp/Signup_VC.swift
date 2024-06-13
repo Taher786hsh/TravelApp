63465e1beb47aa9d8e61afe66e5336a4b95e3df3
Taher786hsh <taherbohra96388@gmail.com>
2024-06-13 09:23:28 +0530

    Initial Commit

---
+++ TravelApp/Signup_VC.swift
@@ -0,0 +1,34 @@
+//
+//  Signup_VC.swift
+//  TravelApp
+//
+//  Created by FCP34 on 12/06/24.
+//
+
+import UIKit
+
+class Signup_VC: UIViewController {
+    
+    
+    @IBAction func ACT_Button(_ sender: Any) {
+    
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TravelApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TravelApp
+//
+//  Created by FCP34 on 12/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+       
+        
+        button.layer.cornerRadius = 45.0
+         
+       
+    }
+
+
+}
